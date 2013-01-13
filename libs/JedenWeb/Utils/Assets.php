0be1e1d1d32a56f1f69c6ca02901b2b264afbff6
PavelJurasek <jurasekpavel@ctyrimedia.cz>
2013-01-13 16:52:05 +0100

    namespace typo

--- libs/JedenWeb/Utils/Assets.php
+++ libs/JedenWeb/Utils/Assets.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
  */
 
-namespace Flame\Utils;
+namespace JedenWeb\Utils;
 
 use Nette;
 
