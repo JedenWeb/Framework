6e84a75f241380dcb0e113c6bf596dcf122a355b
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-02-27 16:14:56 +0100

    remove notes from tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -42,22 +42,3 @@ if (extension_loaded('xdebug')) {
 function id($val) {
 	return $val;
 }
-
-
-class Notes
-{
-	static public $notes = array();
-
-	public static function add($message)
-	{
-		self::$notes[] = $message;
-	}
-
-	public static function fetch()
-	{
-		$res = self::$notes;
-		self::$notes = array();
-		return $res;
-	}
-
-}
