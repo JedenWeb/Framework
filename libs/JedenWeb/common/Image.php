c8366150c3e67a36d656f152bd4badd44ffec7af
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-04-03 12:04:54 +0200

    wip blurring

--- libs/JedenWeb/common/Image.php
+++ libs/JedenWeb/common/Image.php
@@ -138,8 +138,17 @@ class Image extends Nette\Image
 	 * @return Image provides a fluent interface
 	 */
 	public function blur()
-	{
-		imagefilter($this->getImageResource(), IMG_FILTER_GAUSSIAN_BLUR);
+	{		
+		for ($i = 1; $i <= 40; $i++) {
+//			imagefilter($this->getImageResource(), IMG_FILTER_GAUSSIAN_BLUR);
+			$gaussian = array(
+				array(1.0, 2.0, 1.0), 
+				array(2.0, 4.0, 2.0), 
+				array(1.0, 2.0, 1.0)
+			);
+			$div = array_sum(array_map('array_sum', $gaussian));
+			imageconvolution($this->getImageResource(), $gaussian, $div, 0);
+		}
 		return $this;
 	}
 
