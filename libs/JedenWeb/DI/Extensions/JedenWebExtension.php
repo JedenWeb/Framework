a442ea4ee7612949133e2a506633b548ceb9cfe3
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-07-23 16:52:55 +0200

    remove copyright

--- libs/JedenWeb/DI/Extensions/JedenWebExtension.php
+++ libs/JedenWeb/DI/Extensions/JedenWebExtension.php
@@ -1,17 +1,10 @@
 <?php
 
-/**
- * This file is part of the www.jedenweb.cz webpage (http://www.jedenweb.cz/)
- *
- * Copyright (c) 2012 Pavel Jurásek (jurasekpavel@ctyrimedia.cz), Vojtěch Jurásek (jurasek@ctyrimedia.cz)
- *
- * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
- */
-
 namespace JedenWeb\DI\Extensions;
 
 use JedenWeb;
 use Nette;
+use Nette\Utils\Validators;
 
 /**
  * @author Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
