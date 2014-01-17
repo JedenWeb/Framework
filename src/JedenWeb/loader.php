12221dae659d09bbe6549a63f6f5839602dfb9e6
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2014-01-17 11:39:38 +0100

    Added autoload to composer package along with cleaned up loader

--- src/JedenWeb/loader.php
+++ src/JedenWeb/loader.php
@@ -1,30 +1,3 @@
 <?php
 
-namespace JedenWeb;
-
-if (!defined('NETTE')) {
-	$nettePaths = array(
-		__DIR__ . "/../../../../nette/nette/Nette/loader.php",
-		__DIR__ . "/../../../../vendor/nette/nette/Nette/loader.php",
-	);
-	
-	foreach ($nettePaths as $path) {
-		if (file_exists($path)) {
-			$nettePath = $path;
-			break;
-		}
-	}
-	
-	if (!$nettePath) {
-		die('You must load Nette Framework first');
-	}
-
-	include_once $nettePath;
-}
-
-define('JEDENWEB', TRUE);
-define('JEDENWEB_DIR', __DIR__);
-define('JEDENWEB_VERSION', '1.1');
-
-require_once __DIR__ . '/DI/Config/Adapters/NeonAdapter.php';
-require_once __DIR__ . '/common/Configurator.php';
+require_once __DIR__ . '/common/exceptions.php';
